0ef567d79fa71129d2f1eedac59749796d6917d2
Jure Jures <jure.04jj@gmail.com>
2021-04-18 16:31:30 +0200

    locationo model

---
+++ packages/user_info_repository/lib/src/models/location.dart
@@ -0,0 +1,45 @@
+import 'package:equatable/equatable.dart';
+import 'package:user_info_repostiory/src/entity/entity.dart';
+
+class Location extends Equatable {
+  final String country;
+  final String? region;
+  final String? locality;
+
+  const Location({
+    required this.country,
+    this.region,
+    this.locality,
+  });
+
+  @override
+  List<Object?> get props => [country, region, locality];
+
+  Location copyWith({
+    String? country,
+    String? region,
+    String? locality,
+  }) {
+    return Location(
+      country: country ?? this.country,
+      region: region ?? this.region,
+      locality: locality ?? this.locality,
+    );
+  }
+
+  LocationEntity toEntity() {
+    return LocationEntity(
+      country: country,
+      locality: locality,
+      region: region,
+    );
+  }
+
+  factory Location.formEntity(LocationEntity entity) {
+    return Location(
+      country: entity.country,
+      locality: entity.locality,
+      region: entity.region,
+    );
+  }
+}
