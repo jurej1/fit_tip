7d87ba7af203fec98485828236e106a011efe1b0
Jure Jures <jure.04jj@gmail.com>
2021-04-19 10:32:45 +0200

    barell file

---
+++ lib/authentication/blocs/authentication_bloc/authentication_bloc.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'authentication_event.dart';
+part 'authentication_state.dart';
+
+class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> {
+  AuthenticationBloc() : super(AuthenticationState());
+
+  @override
+  Stream<AuthenticationState> mapEventToState(
+    AuthenticationEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
