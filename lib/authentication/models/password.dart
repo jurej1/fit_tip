48a26f8ae0f68a19e1fd8edeba4bfd0e6f3bb2e8
Jure Jures <jure.04jj@gmail.com>
2021-04-20 16:42:52 +0200

    models

---
+++ lib/authentication/models/password.dart
@@ -0,0 +1,19 @@
+import 'package:formz/formz.dart';
+
+enum PasswordValidationError { invalid }
+
+class Password extends FormzInput<String, PasswordValidationError> {
+  const Password.dirty([String val = '']) : super.dirty(val);
+  const Password.pure([String val = '']) : super.pure(val);
+
+  static final _passwordRegex = RegExp(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$');
+
+  @override
+  PasswordValidationError? validator(String value) {
+    if (_passwordRegex.hasMatch(value)) {
+      return null;
+    }
+
+    return PasswordValidationError.invalid;
+  }
+}
