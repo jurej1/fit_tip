4bf7f293058073f0de90b4b69a95777dd06adfcf
Jure Jures <jure.04jj@gmail.com>
2021-04-21 12:18:44 +0200

    info flushbar

--- lib/authentication/widgets/info_flushbar.dart
+++ lib/authentication/widgets/info_flushbar.dart
@@ -1,8 +1,20 @@
 import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 
-void infoAuthFlushbar(
-  BuildContext context,
-) {
-  Flushbar()..show(context);
+void showInfoAuthFlushbar(
+  BuildContext context, {
+  required final String title,
+}) {
+  Flushbar(
+    shouldIconPulse: true,
+    title: title,
+    flushbarPosition: FlushbarPosition.BOTTOM,
+    flushbarStyle: FlushbarStyle.FLOATING,
+    borderRadius: BorderRadius.circular(10),
+    icon: Icon(
+      Icons.info_outline,
+      color: Theme.of(context).primaryColor,
+    ),
+    margin: EdgeInsets.all(8),
+  )..show(context);
 }
