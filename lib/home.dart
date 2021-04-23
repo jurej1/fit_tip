ddd0afd7e2bce2f17b7fd00db508ab571a4447fc
Jure Jures <jure.04jj@gmail.com>
2021-04-23 08:15:23 +0200

    test home

---
+++ lib/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  static const routeName = 'home_route';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Home'),
+      ),
+    );
+  }
+}
