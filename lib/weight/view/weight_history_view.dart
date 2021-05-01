3273df95dd625c7a23e4c2d516a6564f720e35fd
Jure Jures <jure.04jj@gmail.com>
2021-05-01 18:18:55 +0200

    weight history view

---
+++ lib/weight/view/weight_history_view.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class WeightHistoryView extends StatelessWidget {
+  static const routeName = 'weight_history_view';
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: size.height,
+          width: size.width,
+          child: Column(
+            children: [
+              Container(
+                height: 50,
+                width: size.width,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
