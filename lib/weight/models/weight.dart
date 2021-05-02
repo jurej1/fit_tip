03a6b9ea07ca68a158810c47c7558706b3a287b6
Jure Jures <jure.04jj@gmail.com>
2021-05-02 08:39:29 +0200

    weight

---
+++ lib/weight/models/weight.dart
@@ -0,0 +1,25 @@
+import 'package:formz/formz.dart';
+
+enum WeightValidationError { invalid }
+
+class Weight extends FormzInput<String, WeightValidationError> {
+  Weight.dirty([String val = '']) : super.dirty(val);
+  Weight.pure([String val = '']) : super.pure(val);
+
+  @override
+  WeightValidationError? validator(String? value) {
+    if (value == null) {
+      return WeightValidationError.invalid;
+    }
+
+    double? valKg = double.tryParse(value);
+
+    if (valKg == null) {
+      return WeightValidationError.invalid;
+    } else if (valKg.isNegative) {
+      return WeightValidationError.invalid;
+    }
+
+    return null;
+  }
+}
