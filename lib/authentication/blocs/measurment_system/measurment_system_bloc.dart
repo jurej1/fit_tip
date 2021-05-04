f9995ea1d7c44688624f2f764a8d6e620fee1a13
Jure Jures <jure.04jj@gmail.com>
2021-05-04 21:20:39 +0200

    measurment_system_bloc

---
+++ lib/authentication/blocs/measurment_system/measurment_system_bloc.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'measurment_system_event.dart';
+
+class MeasurmentSystemBloc extends Bloc<MeasurmentSystemEvent, MeasurmentSystem> {
+  MeasurmentSystemBloc() : super(MeasurmentSystem.metric);
+
+  @override
+  Stream<MeasurmentSystem> mapEventToState(
+    MeasurmentSystemEvent event,
+  ) async* {
+    if (event is MeasurmentSystemUpdated) {
+      yield event.system;
+    }
+  }
+}
---
+++ lib/authentication/blocs/measurment_system/measurment_system_event.dart
@@ -0,0 +1,17 @@
+part of 'measurment_system_bloc.dart';
+
+abstract class MeasurmentSystemEvent extends Equatable {
+  const MeasurmentSystemEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class MeasurmentSystemUpdated extends MeasurmentSystemEvent {
+  final MeasurmentSystem system;
+
+  MeasurmentSystemUpdated(this.system);
+
+  @override
+  List<Object> get props => [system];
+}
