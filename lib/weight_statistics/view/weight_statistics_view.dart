d2390d07aeaa97562d3c9605a71a0b3ff2add154
Jure Jures <jure.04jj@gmail.com>
2021-05-08 21:26:32 +0200

    app bar

--- lib/weight_statistics/view/weight_statistics_view.dart
+++ lib/weight_statistics/view/weight_statistics_view.dart
@@ -5,6 +5,8 @@ class WeightStatisticsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
