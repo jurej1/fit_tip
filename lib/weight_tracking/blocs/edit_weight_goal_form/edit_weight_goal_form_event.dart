9bddcb0d8685743b60b18b43f7a9333fb0183544
Jure Jures <jure.04jj@gmail.com>
2021-05-10 21:27:01 +0200

    events

--- lib/weight_tracking/blocs/edit_weight_goal_form/edit_weight_goal_form_event.dart
+++ lib/weight_tracking/blocs/edit_weight_goal_form/edit_weight_goal_form_event.dart
@@ -4,5 +4,42 @@ abstract class EditWeightGoalFormEvent extends Equatable {
   const EditWeightGoalFormEvent();
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 }
+
+class EditWeightGoalTargetDateChanged extends EditWeightGoalFormEvent {
+  final DateTime? value;
+
+  const EditWeightGoalTargetDateChanged({this.value});
+  @override
+  List<Object?> get props => [value];
+}
+
+class EditWeightGoalStartDateChanged extends EditWeightGoalFormEvent {
+  final DateTime? value;
+
+  const EditWeightGoalStartDateChanged({this.value});
+
+  @override
+  List<Object?> get props => [value];
+}
+
+class EditWeightGoalStartWeigthChanged extends EditWeightGoalFormEvent {
+  final String? value;
+
+  const EditWeightGoalStartWeigthChanged({this.value});
+
+  @override
+  List<Object?> get props => [value];
+}
+
+class EditWeightGoalTargetWeightGoalChanged extends EditWeightGoalFormEvent {
+  final String? value;
+
+  const EditWeightGoalTargetWeightGoalChanged({this.value});
+
+  @override
+  List<Object?> get props => [value];
+}
+
+class EditWeightGoalFormSubmit extends EditWeightGoalFormEvent {}
