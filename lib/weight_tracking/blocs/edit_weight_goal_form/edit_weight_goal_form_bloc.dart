0e00ea5a2186c5e681514da3fc365328f7341c5c
Jure Jures <jure.04jj@gmail.com>
2021-05-10 21:21:42 +0200

    bloc

--- lib/weight_tracking/blocs/edit_weight_goal_form/edit_weight_goal_form_bloc.dart
+++ lib/weight_tracking/blocs/edit_weight_goal_form/edit_weight_goal_form_bloc.dart
@@ -2,13 +2,17 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:fit_tip/weight_tracking/models/models.dart' as models;
 import 'package:formz/formz.dart';
+import 'package:weight_repository/weight_repository.dart';
 
 part 'edit_weight_goal_form_event.dart';
 part 'edit_weight_goal_form_state.dart';
 
 class EditWeightGoalFormBloc extends Bloc<EditWeightGoalFormEvent, EditWeightGoalFormState> {
-  EditWeightGoalFormBloc() : super(EditWeightGoalFormState());
+  EditWeightGoalFormBloc({
+    required WeightGoal goal,
+  }) : super(EditWeightGoalFormState.pure(goal));
 
   @override
   Stream<EditWeightGoalFormState> mapEventToState(
--- lib/weight_tracking/blocs/edit_weight_goal_form/edit_weight_goal_form_state.dart
+++ lib/weight_tracking/blocs/edit_weight_goal_form/edit_weight_goal_form_state.dart
@@ -3,10 +3,51 @@ part of 'edit_weight_goal_form_bloc.dart';
 class EditWeightGoalFormState extends Equatable {
   const EditWeightGoalFormState({
     this.status = FormzStatus.pure,
+    required this.targetDate,
+    required this.startDate,
+    required this.targetWeight,
+    required this.startWeight,
   });
 
   final FormzStatus status;
+  final models.TargetDate targetDate;
+  final models.StartDate startDate;
+  final models.Weight startWeight;
+  final models.Weight targetWeight;
+
+  factory EditWeightGoalFormState.pure(WeightGoal goal) {
+    return EditWeightGoalFormState(
+      targetDate: models.TargetDate.pure(goal.targetDate),
+      startDate: models.StartDate.pure(goal.beginDate),
+      targetWeight: models.Weight.pure(goal.targetWeight.toString()),
+      startWeight: models.Weight.pure(goal.beginWeight.toString()),
+    );
+  }
 
   @override
-  List<Object> get props => [status];
+  List<Object> get props {
+    return [
+      status,
+      targetDate,
+      startDate,
+      startWeight,
+      targetWeight,
+    ];
+  }
+
+  EditWeightGoalFormState copyWith({
+    FormzStatus? status,
+    models.TargetDate? targetDate,
+    models.StartDate? startDate,
+    models.Weight? startWeight,
+    models.Weight? targetWeight,
+  }) {
+    return EditWeightGoalFormState(
+      status: status ?? this.status,
+      targetDate: targetDate ?? this.targetDate,
+      startDate: startDate ?? this.startDate,
+      startWeight: startWeight ?? this.startWeight,
+      targetWeight: targetWeight ?? this.targetWeight,
+    );
+  }
 }
