959edf725a297db69dabeeae37842da2360b9f4f
Jure Jures <jure.04jj@gmail.com>
2021-05-16 14:08:36 +0200

    enums

---
+++ packages/water_repository/lib/enums/drinking_cup_size.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+
+enum DrinkingCupSize {
+  tee, // 100
+  coffee, // 150
+  small, //300
+  medium, // 400
+  large, //500
+  extraLarge, //600
+}
---
+++ packages/water_repository/lib/enums/enums.dart
@@ -0,0 +1,1 @@
+export 'drinking_cup_size.dart';
