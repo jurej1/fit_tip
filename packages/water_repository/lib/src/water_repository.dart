114e576f366effd2add08e165c7cb6bb1d9c54ef
Jure Jures <jure.04jj@gmail.com>
2021-05-16 17:06:51 +0200

    get water log history

--- packages/water_repository/lib/src/water_repository.dart
+++ packages/water_repository/lib/src/water_repository.dart
@@ -1,9 +1,41 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../entity/water_log_entity.dart';
+import '../models/models.dart';
 
 class WaterRepository {
   WaterRepository({
     FirebaseFirestore? firebaseFirestore,
-  }) : _firebaseFirestore = firebaseFirestore ?? FirebaseFirestore.instance;
+    FirebaseAuth? fireabaseAuth,
+  })  : _firebaseFirestore = firebaseFirestore ?? FirebaseFirestore.instance,
+        _firebaseAuth = fireabaseAuth ?? FirebaseAuth.instance;
 
-  late final FirebaseFirestore _firebaseFirestore;
+  final FirebaseFirestore _firebaseFirestore;
+  final FirebaseAuth _firebaseAuth;
+
+  bool _isAuthenticated() => _firebaseAuth.currentUser != null;
+
+  CollectionReference? _collRef() {
+    if (_firebaseAuth.currentUser != null) {
+      final String userId = _firebaseAuth.currentUser!.uid;
+
+      return _firebaseFirestore.collection('users').doc(userId).collection('water_tracking');
+    }
+    return null;
+  }
+
+  Future<List<WaterLog>?> getWaterLogHistory() async {
+    if (_isAuthenticated()) {
+      QuerySnapshot snapshot = await _collRef()!.get();
+
+      return snapshot.docs.map((e) {
+        final WaterLogEntity entity = WaterLogEntity.fromDocumentSnapshot(e);
+
+        return WaterLog.fromEntity(entity);
+      }).toList();
+    }
+
+    return null;
+  }
 }
--- packages/water_repository/lib/water_repository.dart
+++ packages/water_repository/lib/water_repository.dart
@@ -1,3 +1,5 @@
 library water_repository;
 
 export 'src/water_repository.dart';
+export 'models/models.dart';
+export 'enums/enums.dart';
