52d41641638b30047efad2f2e3a745e47cf2e8ae
Jure Jures <jure.04jj@gmail.com>
2021-05-23 20:44:23 +0200

    state and events

--- lib/water_tracking/blocs/add_water_log_form/add_water_log_form_bloc.dart
+++ lib/water_tracking/blocs/add_water_log_form/add_water_log_form_bloc.dart
@@ -2,13 +2,17 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:fit_tip/water_tracking/models/models.dart';
+import 'package:fit_tip/weight_tracking/models/models.dart';
+import 'package:flutter/material.dart';
 import 'package:formz/formz.dart';
+import 'package:water_repository/enums/enums.dart';
 
 part 'add_water_log_form_event.dart';
 part 'add_water_log_form_state.dart';
 
 class AddWaterLogFormBloc extends Bloc<AddWaterLogFormEvent, AddWaterLogFormState> {
-  AddWaterLogFormBloc() : super(AddWaterLogFormState());
+  AddWaterLogFormBloc() : super(AddWaterLogFormState.pure());
 
   @override
   Stream<AddWaterLogFormState> mapEventToState(
--- lib/water_tracking/blocs/add_water_log_form/add_water_log_form_event.dart
+++ lib/water_tracking/blocs/add_water_log_form/add_water_log_form_event.dart
@@ -4,5 +4,43 @@ abstract class AddWaterLogFormEvent extends Equatable {
   const AddWaterLogFormEvent();
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 }
+
+class AddWaterLogFormDrinkingCupSizeChanged extends AddWaterLogFormEvent {
+  final DrinkingCupSize? size;
+
+  const AddWaterLogFormDrinkingCupSizeChanged({this.size});
+
+  @override
+  List<Object?> get props => [size];
+}
+
+class AddWaterLogFormCupAmountChanged extends AddWaterLogFormEvent {
+  final double? amount;
+
+  const AddWaterLogFormCupAmountChanged({this.amount});
+
+  @override
+  List<Object?> get props => [amount];
+}
+
+class AddWaterLogFormTimeAddedChanged extends AddWaterLogFormEvent {
+  final TimeOfDay? time;
+
+  const AddWaterLogFormTimeAddedChanged({this.time});
+
+  @override
+  List<Object?> get props => [time];
+}
+
+class AddWaterLogFormDateChanged extends AddWaterLogFormEvent {
+  final DateTime? date;
+
+  const AddWaterLogFormDateChanged({this.date});
+
+  @override
+  List<Object?> get props => [date];
+}
+
+class AddWaterLogSubmitForm extends AddWaterLogFormEvent {}
