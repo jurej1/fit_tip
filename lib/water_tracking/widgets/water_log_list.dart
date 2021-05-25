873f37342d6cbcb7d54ad98f39fc3d7991ef8021
Jure Jures <jure.04jj@gmail.com>
2021-05-25 22:21:36 +0200

    icons

--- lib/water_tracking/widgets/water_log_list.dart
+++ lib/water_tracking/widgets/water_log_list.dart
@@ -12,18 +12,31 @@ class WaterLogList extends StatelessWidget {
       return Container();
     }
 
-    return ListView.builder(
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 4,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        childAspectRatio: 2 / 3,
+      ),
       shrinkWrap: true,
       physics: const ClampingScrollPhysics(),
       itemCount: waterLogs.length,
       itemBuilder: (context, index) {
         final item = waterLogs[index];
-        return ListTile(
-          title: Text(
-            item.time.format(context),
-          ),
-          trailing: Text(
-            item.cup.amount.toInt().toString(),
+        return Card(
+          elevation: 1.5,
+          color: Colors.blue[300],
+          child: GridTile(
+            header: Text(
+              item.time.format(context),
+              textAlign: TextAlign.center,
+            ),
+            child: Center(),
+            footer: Text(
+              item.cup.amount.toInt().toString(),
+              textAlign: TextAlign.center,
+            ),
           ),
         );
       },
