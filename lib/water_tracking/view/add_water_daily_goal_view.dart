197d4524c003a9f9bc69aa67833de63cfc82746e
Jure Jures <jure.04jj@gmail.com>
2021-05-27 21:25:02 +0200

    new view

---
+++ lib/water_tracking/view/add_water_daily_goal_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddWaterDailyGoalView extends StatelessWidget {
+  static const routeName = 'add_water_daily_goal_view';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/water_tracking/view/view.dart
+++ lib/water_tracking/view/view.dart
@@ -1,1 +1,2 @@
 export 'water_log_view.dart';
+export 'add_water_daily_goal_view.dart';
