ffbeee6b3c0dab54c09b93c8b8aee7225fb3857b
Jure Jures <jure.04jj@gmail.com>
2021-06-12 11:55:23 +0200

    cleanup

---
+++ lib/food_tracking/view/edit_calorie_daily_goal_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EditCalorieDailyGoalView extends StatelessWidget {
+  const EditCalorieDailyGoalView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit calorie daily goal'),
+      ),
+    );
+  }
+}
--- lib/food_tracking/view/view.dart
+++ lib/food_tracking/view/view.dart
@@ -1,2 +1,3 @@
 export 'food_daily_logs_view.dart';
 export 'add_food_log_view.dart';
+export 'edit_calorie_daily_goal_view.dart';
