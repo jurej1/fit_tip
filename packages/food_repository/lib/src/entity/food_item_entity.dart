a53ecb9ac8d1fd1849ca5854b75fa6207056903f
Jure Jures <jure.04jj@gmail.com>
2021-06-18 19:09:45 +0200

    cleanup

--- packages/food_repository/lib/src/entity/food_item_entity.dart
+++ packages/food_repository/lib/src/entity/food_item_entity.dart
@@ -4,6 +4,17 @@ import 'package:flutter/foundation.dart';
 import 'package:food_repository/src/entity/entity.dart';
 import 'package:food_repository/src/enums/enums.dart';
 
+class _DocKeys {
+  static String name = 'name';
+  static String sourceRef = 'sourceRef';
+  static String amount = 'amount';
+  static String calories = 'calories';
+  static String dateAdded = 'dateAdded';
+  static String mealType = 'mealType';
+  static String macronutrients = 'macronutrients';
+  static String vitamins = 'vitamins';
+}
+
 class FoodItemEntity extends Equatable {
   final String? id;
   final String name;
@@ -44,14 +55,14 @@ class FoodItemEntity extends Equatable {
 
   Map<String, dynamic> toDocumentSnapshot() {
     return {
-      'name': name,
-      'sourceRef': sourceRef,
-      'amount': amount,
-      'calories': calories,
-      'dateAdded': Timestamp.fromDate(dateAdded),
-      'mealType': describeEnum(mealType),
-      if (macronutrients != null) 'macronutrients': macronutrients!.map((e) => e.toDocumentSnapshot()).toList(),
-      if (vitamins != null) 'vitamins': vitamins!.map((e) => e.toDocumentSnapshot()).toList()
+      _DocKeys.name: name,
+      _DocKeys.sourceRef: sourceRef,
+      _DocKeys.amount: amount,
+      _DocKeys.calories: calories,
+      _DocKeys.dateAdded: Timestamp.fromDate(dateAdded),
+      _DocKeys.mealType: describeEnum(mealType),
+      if (macronutrients != null) _DocKeys.macronutrients: macronutrients!.map((e) => e.toDocumentSnapshot()).toList(),
+      if (vitamins != null) _DocKeys.vitamins: vitamins!.map((e) => e.toDocumentSnapshot()).toList()
     };
   }
 
@@ -59,18 +70,19 @@ class FoodItemEntity extends Equatable {
     final data = snap.data() as Map<String, dynamic>;
 
     return FoodItemEntity(
-      amount: data['amount'],
-      calories: data['calories'],
-      dateAdded: (data['dateAdded'] as Timestamp).toDate(),
+      amount: data[_DocKeys.amount],
+      calories: data[_DocKeys.calories],
+      dateAdded: (data[_DocKeys.dateAdded] as Timestamp).toDate(),
       id: snap.id,
-      name: data['name'],
-      sourceRef: data['sourceRef'],
-      mealType: MealType.values.firstWhere((e) => describeEnum(e) == data['mealType']),
-      macronutrients: data.containsKey('macronutrients')
-          ? (data['macronutrients'] as List<dynamic>).map((e) => FoodDataMacroEntity.fromMap(e)).toList()
+      name: data[_DocKeys.name],
+      sourceRef: data[_DocKeys.sourceRef],
+      mealType: MealType.values.firstWhere((e) => describeEnum(e) == data[_DocKeys.mealType]),
+      macronutrients: data.containsKey(_DocKeys.macronutrients)
+          ? (data[_DocKeys.macronutrients] as List<dynamic>).map((e) => FoodDataMacroEntity.fromMap(e)).toList()
           : null,
-      vitamins:
-          data.containsKey('vitamins') ? (data['vitamins'] as List<dynamic>).map((e) => FoodDataVitaminEntity.fromMap(e)).toList() : null,
+      vitamins: data.containsKey(_DocKeys.vitamins)
+          ? (data[_DocKeys.vitamins] as List<dynamic>).map((e) => FoodDataVitaminEntity.fromMap(e)).toList()
+          : null,
     );
   }
 
