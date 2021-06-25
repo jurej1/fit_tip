a5f96f25b18e9c728a1238e182664208d3168018
Jure Jures <jure.04jj@gmail.com>
2021-06-25 17:07:32 +0200

    cleanup

--- lib/food_tracking/widgets/food_log_day_selector.dart
+++ lib/food_tracking/widgets/food_log_day_selector.dart
@@ -1,3 +1,4 @@
+import 'package:fit_tip/authentication/authentication.dart';
 import 'package:fit_tip/food_tracking/food_tracking.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -29,7 +30,7 @@ class FoodLogDaySelector extends StatelessWidget {
                 DateTime? date = await showDatePicker(
                   context: context,
                   initialDate: state.selectedDate,
-                  firstDate: state.selectedDate.subtract(const Duration(days: 365)),
+                  firstDate: BlocProvider.of<AuthenticationBloc>(context).state.user!.dateJoined!,
                   lastDate: DateTime.now(),
                 );
 
