f7123301652b7e5c8a901c485bc7feb952aeecac
Jure <jure.04jj@gmail.com>
2021-06-30 09:19:06 +0200

    cleanup

--- lib/water_tracking/widgets/water_log_day_selector.dart
+++ lib/water_tracking/widgets/water_log_day_selector.dart
@@ -1,6 +1,6 @@
 import 'package:fit_tip/authentication/authentication.dart';
+import 'package:fit_tip/shared/shared.dart';
 import 'package:fit_tip/water_tracking/water_tracking.dart';
-import 'package:fit_tip/widgets/day_selector.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
