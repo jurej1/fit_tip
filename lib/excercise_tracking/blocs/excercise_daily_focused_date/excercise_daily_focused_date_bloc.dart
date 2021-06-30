9500664680052074671b4c94f05003ad8f073d41
Jure <jure.04jj@gmail.com>
2021-06-30 08:18:27 +0200

    excercise daily focused date

--- lib/excercise_tracking/blocs/excercise_daily_focused_date/excercise_daily_focused_date_bloc.dart
+++ lib/excercise_tracking/blocs/excercise_daily_focused_date/excercise_daily_focused_date_bloc.dart
@@ -12,5 +12,13 @@ class ExcerciseDailyFocusedDateBloc extends Bloc<ExcerciseDailyFocusedDateEvent,
   @override
   Stream<ExcerciseDailyFocusedDateState> mapEventToState(
     ExcerciseDailyFocusedDateEvent event,
-  ) async* {}
+  ) async* {
+    if (event is ExcerciseDailyFocusedDateNextDayPressed) {
+      if (!state.isSelectedDateToday()) {
+        yield state.copyWith(selectedDate: this.state.selectedDate.add(const Duration(days: 1)));
+      }
+    } else if (event is ExcerciseDailyFocusedDatePreviousDayPressed) {
+      yield state.copyWith(selectedDate: this.state.selectedDate.subtract(const Duration(days: 1)));
+    }
+  }
 }
