4a7e8292b772da1ddb21c7c10829ea6f3fa389cc
Jure <jure.04jj@gmail.com>
2021-07-02 16:35:09 +0200

    cleanup

--- lib/excercise_tracking/view/add_excercise_log_view.dart
+++ lib/excercise_tracking/view/add_excercise_log_view.dart
@@ -2,9 +2,11 @@ import 'package:activity_repository/activity_repository.dart';
 import 'package:fit_tip/authentication/authentication.dart';
 import 'package:fit_tip/excercise_tracking/blocs/blocs.dart';
 import 'package:fit_tip/excercise_tracking/widgets/widgets.dart';
+import 'package:fit_tip/food_tracking/food_tracking.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:formz/formz.dart';
+import 'package:intl/intl.dart';
 
 class AddExcerciseLogView extends StatelessWidget {
   const AddExcerciseLogView({Key? key}) : super(key: key);
@@ -50,6 +52,9 @@ class AddExcerciseLogView extends StatelessWidget {
               _wrappedPadding(const _ExcerciseNameInput()),
               const _DurationInput(),
               _wrappedPadding(const IntensityInput()),
+              _wrappedPadding(const _CaloriesInput()),
+              _wrappedPadding(const _TimeInput()),
+              _wrappedPadding(const _DateInput()),
             ],
           );
         },
@@ -101,6 +106,75 @@ class _ExcerciseNameInput extends StatelessWidget {
             labelText: 'Excercise name',
           ),
           onChanged: (val) => BlocProvider.of<AddExcerciseLogBloc>(context).add(AddExcerciseLogNameUpdated(val)),
+        );
+      },
+    );
+  }
+}
+
+class _CaloriesInput extends StatelessWidget {
+  const _CaloriesInput({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<AddExcerciseLogBloc, AddExcerciseLogState>(
+      builder: (context, state) {
+        return RowInputField(
+          initialValue: state.calories.value,
+          onChanged: (value) {
+            BlocProvider.of<AddExcerciseLogBloc>(context).add(AddExcerciseLogCaloriesUpdated(value));
+          },
+          unit: 'cal',
+          title: 'Calories',
+        );
+      },
+    );
+  }
+}
+
+class _TimeInput extends StatelessWidget {
+  const _TimeInput({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<AddExcerciseLogBloc, AddExcerciseLogState>(
+      builder: (context, state) {
+        return ListTile(
+          contentPadding: EdgeInsets.zero,
+          title: Text('Start time: '),
+          trailing: Text(
+            state.time.value.format(context),
+          ),
+          onTap: () async {
+            TimeOfDay? time = await showTimePicker(context: context, initialTime: state.time.value);
+            BlocProvider.of<AddExcerciseLogBloc>(context).add(AddExcerciseLogTimeUpdated(time));
+          },
+        );
+      },
+    );
+  }
+}
+
+class _DateInput extends StatelessWidget {
+  const _DateInput({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<AddExcerciseLogBloc, AddExcerciseLogState>(
+      builder: (context, state) {
+        return ListTile(
+          title: Text('Date: '),
+          trailing: Text(
+            DateFormat('dd.MMMM.yyyy').format(state.date.value),
+          ),
+          onTap: () async {
+            DateTime? date = await showDatePicker(
+              context: context,
+              initialDate: state.date.value,
+              firstDate: BlocProvider.of<AuthenticationBloc>(context).state.user!.dateJoined!,
+              lastDate: DateTime.now(),
+            );
+          },
         );
       },
     );
