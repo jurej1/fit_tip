7f9bc00638cefa348f7d5ff5dd21bc2d9f7ce3ff
Jure <jure.04jj@gmail.com>
2021-07-04 21:06:33 +0200

    cleanup

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,4 @@
-import 'package:fit_tip/widgets/app_drawer.dart';
+import 'package:fit_tip/shared/widgets/app_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
