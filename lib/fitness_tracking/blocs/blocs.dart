9dea2a58b09c670e616b29f6996ba51e668809f6
Jure <jure.04jj@gmail.com>
2021-07-09 14:32:55 +0200

    fitness tracking view cubit

--- lib/fitness_tracking/blocs/blocs.dart
+++ lib/fitness_tracking/blocs/blocs.dart
@@ -1,1 +1,1 @@
-
+export 'fitness_tracking_view/fitness_tracking_view_cubit.dart';
---
+++ lib/fitness_tracking/blocs/fitness_tracking_view/fitness_tracking_view_cubit.dart
@@ -0,0 +1,11 @@
+import 'package:bloc/bloc.dart';
+
+enum FitnessTrackingWorkoutPage { active, all }
+
+class FitnessTrackingViewCubit extends Cubit<FitnessTrackingWorkoutPage> {
+  FitnessTrackingViewCubit() : super(FitnessTrackingWorkoutPage.active);
+
+  void viewUpdated(int index) {
+    emit(FitnessTrackingWorkoutPage.values.elementAt(index));
+  }
+}
