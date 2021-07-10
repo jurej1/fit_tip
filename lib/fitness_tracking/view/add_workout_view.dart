a80864901b429f9f94bca37ff495c1db0de952a7
Jure <jure.04jj@gmail.com>
2021-07-10 19:02:17 +0200

    cleanup

--- lib/fitness_tracking/view/add_workout_view.dart
+++ lib/fitness_tracking/view/add_workout_view.dart
@@ -1,5 +1,4 @@
 import 'package:fit_tip/fitness_tracking/fitness_tracking.dart';
-import 'package:fit_tip/food_tracking/food_tracking.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
