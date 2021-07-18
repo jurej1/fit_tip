e5bc9665f3a1d31e13512f4c27767535a0d69446
Jure <jure.04jj@gmail.com>
2021-07-18 15:57:07 +0200

    updating

--- lib/fitness_tracking/view/add_workout_view.dart
+++ lib/fitness_tracking/view/add_workout_view.dart
@@ -1,5 +1,6 @@
 import 'package:fit_tip/authentication/authentication.dart';
 import 'package:fit_tip/fitness_tracking/fitness_tracking.dart';
+import 'package:fit_tip/shared/shared.dart';
 import 'package:fitness_repository/fitness_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -43,11 +44,14 @@ class AddWorkoutView extends StatelessWidget {
       appBar: AddWorkoutViewAppBar(),
       body: BlocConsumer<AddWorkoutFormBloc, AddWorkoutFormState>(
         listener: (context, state) {
-          if (state.status.isSubmissionSuccess) {
+          if (state.status.isSubmissionSuccess && state.formMode == FormMode.add) {
             ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Workout added')));
             BlocProvider.of<WorkoutsListBloc>(context).add(WorkoutsListItemAdded(state.workout));
-            Navigator.of(context).pop();
           }
+          if (state.status.isSubmissionSuccess && state.formMode == FormMode.edit) {
+            BlocProvider.of<WorkoutsListBloc>(context).add(WorkoutsListItemUpdated(state.workout));
+          }
+          Navigator.of(context).pop();
         },
         builder: (context, state) {
           if (state.status.isSubmissionInProgress) {
