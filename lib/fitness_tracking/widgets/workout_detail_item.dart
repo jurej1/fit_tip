aff6141a00e72635e8cbb54487a35031556d2e1e
Jure <jure.04jj@gmail.com>
2021-07-21 11:04:01 +0200

    cleanup

--- lib/fitness_tracking/widgets/workout_detail_item.dart
+++ lib/fitness_tracking/widgets/workout_detail_item.dart
@@ -13,46 +13,71 @@ class WorkoutDetailItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        if (workout.excercises.isNotEmpty) Text(workout.mapDayToText),
-        if (workout.musclesTargeted != null)
-          ...workout.musclesTargeted!
-              .map(
-                (e) => Chip(
-                  label: Text(mapMuscleGroupToString(e)),
-                ),
-              )
-              .toList(),
-        if (workout.excercises.isNotEmpty) ...{
-          Row(
-            children: [
-              WorkoutExcerciseRowData(
-                text: 'Name',
-                style: TextStyle(fontWeight: FontWeight.w600),
-              ),
-              WorkoutExcerciseRowData(
-                text: 'Sets',
-                style: TextStyle(fontWeight: FontWeight.w600),
-              ),
-              WorkoutExcerciseRowData(
-                text: 'Reps',
-                style: TextStyle(fontWeight: FontWeight.w600),
-              ),
-            ],
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.blue.shade100,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            workout.mapDayToText,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        },
-        ...workout.excercises.map((e) {
-          return Row(
-            children: [
-              WorkoutExcerciseRowData(text: e.name),
-              WorkoutExcerciseRowData(text: e.setsString),
-              WorkoutExcerciseRowData(text: e.repsString),
-            ],
-          );
-        }).toList(),
-      ],
+          if (workout.musclesTargeted != null) ...{
+            SizedBox(height: 5),
+            Container(
+              height: 1.5,
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              color: Colors.black26,
+            ),
+            ...workout.musclesTargeted!
+                .map(
+                  (e) => Chip(
+                    label: Text(
+                      mapMuscleGroupToString(e),
+                      style: TextStyle(color: Colors.grey.shade100),
+                    ),
+                    backgroundColor: Colors.blueAccent.shade100,
+                  ),
+                )
+                .toList(),
+          },
+          if (workout.excercises.isNotEmpty) ...{
+            Row(
+              children: [
+                WorkoutExcerciseRowData(
+                  text: 'Name',
+                  style: TextStyle(fontWeight: FontWeight.w600),
+                ),
+                WorkoutExcerciseRowData(
+                  text: 'Sets',
+                  style: TextStyle(fontWeight: FontWeight.w600),
+                ),
+                WorkoutExcerciseRowData(
+                  text: 'Reps',
+                  style: TextStyle(fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+            SizedBox(height: 5),
+          },
+          ...workout.excercises.map((e) {
+            return Row(
+              children: [
+                WorkoutExcerciseRowData(text: e.name),
+                WorkoutExcerciseRowData(text: e.setsString),
+                WorkoutExcerciseRowData(text: e.repsString),
+              ],
+            );
+          }).toList(),
+        ],
+      ),
     );
   }
 }
