716e55bc74185153cc010784aec74fda2935fe6f
Jure <jure.04jj@gmail.com>
2021-08-10 17:33:25 +0200

    cleanup

--- lib/fitness_tracking/widgets/active_workout_builder.dart
+++ lib/fitness_tracking/widgets/active_workout_builder.dart
@@ -1,6 +1,5 @@
 import 'package:fit_tip/food_tracking/widgets/widgets.dart';
 import 'package:fit_tip/shared/shared.dart';
-import 'package:fitness_repository/fitness_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
