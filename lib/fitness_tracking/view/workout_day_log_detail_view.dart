52b57a3054625fd59a00d743a3fb224bf44536ac
Jure <jure.04jj@gmail.com>
2021-08-12 07:40:17 +0200

    workout day log detail view

--- lib/fitness_tracking/view/workout_day_log_detail_view.dart
+++ lib/fitness_tracking/view/workout_day_log_detail_view.dart
@@ -2,6 +2,7 @@ import 'package:fit_tip/authentication/authentication.dart';
 import 'package:fitness_repository/fitness_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 
 import '../fitness_tracking.dart';
 
@@ -25,32 +26,57 @@ class WorkoutDayLogDetailView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Workout details'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              BlocProvider.of<WorkoutDayLogDetailBloc>(context).add(WorkoutDayLogDetailDeleteRequested());
-            },
-            icon: const Icon(Icons.delete),
-          ),
-        ],
-      ),
-      body: BlocBuilder<WorkoutDayLogDetailBloc, WorkoutDayLogDetailState>(
-        builder: (context, state) {
-          if (state is WorkoutDayLogDetailLoading) {
-            return Center(
-              child: const CircularProgressIndicator(),
-            );
-          }
+    return BlocListener<WorkoutDayLogDetailBloc, WorkoutDayLogDetailState>(
+      listener: (context, state) {
+        if (state is WorkoutDayLogDetailDeleteSuccess) {
+          //TODO update the list
+          Navigator.of(context).pop();
+        } else if (state is WorkoutDayLogDetailFail) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Sorry there was an error')));
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Workout details'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                BlocProvider.of<WorkoutDayLogDetailBloc>(context).add(WorkoutDayLogDetailDeleteRequested());
+              },
+              icon: const Icon(Icons.delete),
+            ),
+          ],
+        ),
+        body: BlocBuilder<WorkoutDayLogDetailBloc, WorkoutDayLogDetailState>(
+          builder: (context, state) {
+            if (state is WorkoutDayLogDetailLoading) {
+              return Center(
+                child: const CircularProgressIndicator(),
+              );
+            }
 
-          return Column(
-            children: [
-              Text('Workout data'),
-            ],
-          );
-        },
+            return SingleChildScrollView(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Created on ${DateFormat('EEE, MMM d ' 'yy').format(state.dayLog.created)}',
+                  ),
+                  const SizedBox(height: 10),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: state.dayLog.excercises.length,
+                    itemBuilder: (context, index) {
+                      final item = state.dayLog.excercises[index];
+                      return WorkoutExcerciseCard.provider(item);
+                    },
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
