bc7bacd569a45bde04e3ee3f32b438d0bfaa0c23
Jure <jure.04jj@gmail.com>
2021-08-23 18:05:20 +0200

    clenaup

--- lib/fitness_blogs/view/blog_posts_view.dart
+++ lib/fitness_blogs/view/blog_posts_view.dart
@@ -16,7 +16,7 @@ class BlogPostsView extends StatelessWidget {
               create: (context) => BlogPostsListBloc(
                 authenticationBloc: BlocProvider.of<AuthenticationBloc>(context),
                 blogRepository: RepositoryProvider.of<BlogRepository>(context),
-              ),
+              )..add(BlogPostsListLoadRequested()),
             )
           ],
           child: BlogPostsView(),
@@ -27,9 +27,44 @@ class BlogPostsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text('Blogs view'),
+      ),
+      body: BlocBuilder<BlogPostsListBloc, BlogPostsListState>(
+        builder: (context, state) {
+          if (state is BlogPostsListLoading) {
+            return Center(
+              child: const CircularProgressIndicator(),
+            );
+          } else if (state is BlogPostsListLoadSuccess) {
+            final length = state.hasReachedMax ? state.blogs.length : state.blogs.length + 1;
+            return ListView.separated(
+              itemCount: length,
+              itemBuilder: (context, index) {
+                if (length - 1 == index && !state.hasReachedMax) {
+                  return Container(
+                    height: 50,
+                    width: size.width,
+                    child: const Center(
+                      child: const CircularProgressIndicator(),
+                    ),
+                  );
+                }
+
+                final item = state.blogs[index];
+                return ListTile(
+                  title: Text(item.title),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(height: 10);
+              },
+            );
+          }
+          return Container();
+        },
       ),
     );
   }
