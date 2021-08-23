36645afa56625df6800f1fc2cb1171cbc3e33a44
Jure <jure.04jj@gmail.com>
2021-08-23 20:05:01 +0200

    provider

--- lib/fitness_blogs/view/add_blog_post_form_view.dart
+++ lib/fitness_blogs/view/add_blog_post_form_view.dart
@@ -1,4 +1,8 @@
+import 'package:blog_repository/blog_repository.dart';
+import 'package:fit_tip/authentication/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:fit_tip/fitness_blogs/blocs/blocs.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AddBlogPostFormView extends StatelessWidget {
   const AddBlogPostFormView({Key? key}) : super(key: key);
@@ -6,15 +10,25 @@ class AddBlogPostFormView extends StatelessWidget {
   static MaterialPageRoute route(BuildContext context) {
     return MaterialPageRoute(
       builder: (_) {
-        return AddBlogPostFormView();
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) => AddBlogPostBloc(
+                authenticationBloc: BlocProvider.of<AuthenticationBloc>(context),
+                blogRepository: RepositoryProvider.of<BlogRepository>(context),
+              ),
+            ),
+          ],
+          child: AddBlogPostFormView(),
+        );
       },
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      appBar: AppBar(),
     );
   }
 }
