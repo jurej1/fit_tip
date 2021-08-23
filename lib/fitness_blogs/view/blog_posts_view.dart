a6a22357e5652f20cb767b8e64ac8ec3bd95ee1e
Jure <jure.04jj@gmail.com>
2021-08-23 10:28:21 +0200

    provider

--- lib/fitness_blogs/view/blog_posts_view.dart
+++ lib/fitness_blogs/view/blog_posts_view.dart
@@ -1,4 +1,8 @@
+import 'package:blog_repository/blog_repository.dart';
+import 'package:fit_tip/authentication/authentication.dart';
+import 'package:fit_tip/fitness_blogs/fitness_blogs.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BlogPostsView extends StatelessWidget {
   const BlogPostsView({Key? key}) : super(key: key);
@@ -6,7 +10,17 @@ class BlogPostsView extends StatelessWidget {
   static MaterialPageRoute route(BuildContext context) {
     return MaterialPageRoute(
       builder: (_) {
-        return BlogPostsView();
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) => BlogPostsListBloc(
+                authenticationBloc: BlocProvider.of<AuthenticationBloc>(context),
+                blogRepository: RepositoryProvider.of<BlogRepository>(context),
+              ),
+            )
+          ],
+          child: BlogPostsView(),
+        );
       },
     );
   }
