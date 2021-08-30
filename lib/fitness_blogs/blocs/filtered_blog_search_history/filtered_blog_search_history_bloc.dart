2d45aa0f54efb1c962779431b90ecda1470eab7c
Jure <jure.04jj@gmail.com>
2021-08-30 19:00:55 +0200

    clenup

--- lib/fitness_blogs/blocs/filtered_blog_search_history/filtered_blog_search_history_bloc.dart
+++ lib/fitness_blogs/blocs/filtered_blog_search_history/filtered_blog_search_history_bloc.dart
@@ -11,28 +11,39 @@ part 'filtered_blog_search_history_state.dart';
 class FilteredBlogSearchHistoryBloc extends Bloc<FilteredBlogSearchHistoryEvent, FilteredBlogSearchHistoryState> {
   FilteredBlogSearchHistoryBloc({
     required BlogSearchHistoryBloc blogSearchHistoryBloc,
-    required SearchBy searchBy,
+    required SearchBloc searchBloc,
   })  : _blogSearchHistoryBloc = blogSearchHistoryBloc,
         super(
           FilteredBlogSearchHistoryState(
-            values: blogSearchHistoryBloc.state.getValuesBySearchBy(searchBy),
+            values: blogSearchHistoryBloc.state.getValuesBySearchBy(searchBloc.state.searchBy),
           ),
-        );
+        ) {
+    _searchSubscription = searchBloc.stream.listen((searchState) {
+      add(_FilteredBlogSearchHistoryQueryUpdated(searchState.search.value));
+    });
+  }
 
   final BlogSearchHistoryBloc _blogSearchHistoryBloc;
+  late final StreamSubscription _searchSubscription;
+
+  @override
+  Future<void> close() {
+    _searchSubscription.cancel();
+    return super.close();
+  }
 
   @override
   Stream<FilteredBlogSearchHistoryState> mapEventToState(
     FilteredBlogSearchHistoryEvent event,
   ) async* {
-    if (event is FilteredBlogSearchHistoryQueryUpdated) {
+    if (event is _FilteredBlogSearchHistoryQueryUpdated) {
       yield* _mapQueryUpdatedToState(event);
     } else if (event is FilteredBlogSearchHistorySearchByUpdated) {
       yield* _mapSearchByUpdatedToState(event);
     }
   }
 
-  Stream<FilteredBlogSearchHistoryState> _mapQueryUpdatedToState(FilteredBlogSearchHistoryQueryUpdated event) async* {
+  Stream<FilteredBlogSearchHistoryState> _mapQueryUpdatedToState(_FilteredBlogSearchHistoryQueryUpdated event) async* {
     List<String> currentValues = List.from(state.values);
 
     String lowerQuery = event.value.toLowerCase();
