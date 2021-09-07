f231dea9537332529b6cf0443ac2dee0d52ad29b
Jure <jure.04jj@gmail.com>
2021-09-07 16:55:39 +0200

    workouts view builder

--- lib/fitness_tracking/view/fitness_workouts_view.dart
+++ lib/fitness_tracking/view/fitness_workouts_view.dart
@@ -1,4 +1,5 @@
 import 'package:fit_tip/authentication/authentication.dart';
+import 'package:fit_tip/fitness_tracking/widgets/user_workouts_list_builder.dart';
 import 'package:fitness_repository/fitness_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -26,7 +27,7 @@ class FitnessWorkoutsView extends StatelessWidget {
               create: (context) => UserWorkoutsListBloc(
                 authenticationBloc: BlocProvider.of<AuthenticationBloc>(context),
                 fitnessRepository: RepositoryProvider.of<FitnessRepository>(context),
-              ),
+              )..add(WorkoutInfosLoadRequested()),
             ),
           ],
           child: FitnessWorkoutsView(),
@@ -49,7 +50,13 @@ class FitnessWorkoutsView extends StatelessWidget {
           )
         ],
       ),
-      body: AllWorkoutsListBuilder(),
+      body: BlocBuilder<FitnessWorkoutsViewSelectorCubit, FitnessWorkoutsViewSelectorState>(
+        builder: (context, state) {
+          if (state.isAll) return AllWorkoutsListBuilder();
+          if (state.isUser) return UserWorkoutsListBuilder();
+          return Container();
+        },
+      ),
       bottomNavigationBar: FitnessWorkoutsViewSelector(),
     );
   }
